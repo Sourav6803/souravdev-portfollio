1f41847e14ec5efb7b158ab36dce6b2b26e9203d
Sourav6803 <souravbhukta8@gmail.com>
2026-02-04 22:53:46 +0530

    deployed URL add

--- src/server.js
+++ src/server.js
@@ -1,1 +1,3 @@
-export const server = 'http://localhost:8000/api/v1'
+// export const server = 'http://localhost:8000/api/v1'
+
+export const server = 'https://portfollio-backend-v3xb.onrender.com/api/v1'
